8437f171a2b97e89af7d8e148128c97923f11cd2
Amazinghua <904873796@qq.com>
2017-01-26 17:03:30 +0800

    new pic

--- scripts/showPic.js
+++ scripts/showPic.js
@@ -1,5 +1,6 @@
- function ShowPic(Whichpic)
+ function ShowPic(Whichpic) {
         whichpic.getAttribute("href")
         var source = whichpic.getAttribute("href")
         var placeholder = document.getElementById("placeholder");
         placeholder.setAttribute("src",source);
+ }
