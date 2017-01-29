26939652f4689dc03e250e7d41a9e2c0c0e33fdc
Amazinghua <904873796@qq.com>
2017-01-29 16:28:42 +0800

    arr map set

---
+++ Arr map set.js
@@ -0,0 +1,25 @@
+var a = ['A', 'B', 'C'];
+var s = new Set(['A', 'B', 'C']);
+var m = new Map([[1, 'x'], [2, 'y'], [3, 'z']]);
+for (var x of a) { // 遍历Array
+    alert(x);
+}
+for (var x of s) { // 遍历Set
+    alert(x);
+}
+for (var x of m) { // 遍历Map
+    alert(x[0] + '=' + x[1]);
+}
+
+
+var a = ['A', 'B', 'C'];
+a.name = 'Hello';
+for (var x in a) {
+    alert(x); // '0', '1', '2', 'name'
+}
+
+var a = ['A', 'B', 'C'];
+a.name = 'Hello';
+for (var x of a) {
+    alert(x); // 'A', 'B', 'C'
+}
