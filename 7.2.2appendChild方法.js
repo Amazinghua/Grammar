a816822cdba70c549338fbaadf1a2029bf6fb39a
Amazinghua <904873796@qq.com>
2017-02-17 10:32:26 +0800

    7.2

---
+++ 7.2.2appendChild方法.js
@@ -0,0 +1,5 @@
+var para = document.creatElement("p");
+var testdiv = document.getElementById("testdiv");
+testdiv.appendChild(para);
+// document.getElementById("testdiv").appendChild(document.creatElement("p"));
+//
